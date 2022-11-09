7060a5f5c25582d8c39b3f508be8e2b7d21c0346
[1O16] <youiiyou1253@gmail.com>
2022-11-10 01:30:05 +0900

    usePreventLeave

---
+++ useConfirm/useConfirm.js
@@ -0,0 +1,17 @@
+const useConfirm = (message = "", onConfirm, onCancel) => {
+  if (typeof onConfirm !== "function") {
+    return;
+  }
+  const confirmAction = () => {
+    if (window.confirm(message)) {
+      onConfirm();
+    } else {
+      try {
+        onCancel();
+      } catch (error) {
+        return;
+      }
+    }
+  };
+  return confirmAction;
+};
